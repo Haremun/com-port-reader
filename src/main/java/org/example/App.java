93bc741800c6634e59488922afb3ca4135ee8e41
Kamil <kamilbieg96@gmail.com>
2023-04-26 22:47:05 +0200

    Data writen and recived

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,18 +4,12 @@ import com.fazecast.jSerialComm.SerialPort;
 import com.fazecast.jSerialComm.SerialPortDataListener;
 import com.fazecast.jSerialComm.SerialPortEvent;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 
 /**
  * Hello world!
  */
 public class App {
-
-    private static final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
 
     public static void main(String[] args) {
         SerialPort[] ports = SerialPort.getCommPorts();
@@ -35,7 +29,20 @@ public class App {
                     System.out.println(serialPortEvent.getSerialPort().getSystemPortName() + ": " + new String(receivedData));
                 }
             });
+            port.addDataListener(new SerialPortDataListener() {
+                @Override
+                public int getListeningEvents() {
+                    return SerialPort.LISTENING_EVENT_DATA_WRITTEN;
+                }
+
+                @Override
+                public void serialEvent(SerialPortEvent serialPortEvent) {
+                    byte[] receivedData = serialPortEvent.getReceivedData();
+                    System.out.println(serialPortEvent.getSerialPort().getSystemPortName() + ": " + new String(receivedData));
+                }
+            });
         }
-        while (true) {}
+        while (true) {
+        }
     }
 }
