b3497e89c463cb3d9beb5996946b7a56419096d0
Kamil <kamilbieg96@gmail.com>
2023-04-26 22:37:44 +0200

    Reading ports

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,11 @@ import com.fazecast.jSerialComm.SerialPort;
 import com.fazecast.jSerialComm.SerialPortDataListener;
 import com.fazecast.jSerialComm.SerialPortEvent;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Hello world!
@@ -18,26 +19,23 @@ public class App {
 
     public static void main(String[] args) {
         SerialPort[] ports = SerialPort.getCommPorts();
-
         System.out.println(Arrays.toString(ports));
 
-        executorService.schedule(() -> {
-            for (SerialPort port : ports) {
-                port.addDataListener(new SerialPortDataListener() {
-                    @Override
-                    public int getListeningEvents() {
-                        return SerialPort.LISTENING_EVENT_DATA_WRITTEN;
-                    }
+        for (SerialPort port : ports) {
+            port.openPort();
+            port.addDataListener(new SerialPortDataListener() {
+                @Override
+                public int getListeningEvents() {
+                    return SerialPort.LISTENING_EVENT_DATA_RECEIVED;
+                }
 
-                    @Override
-                    public void serialEvent(SerialPortEvent serialPortEvent) {
-                        byte[] receivedData = serialPortEvent.getReceivedData();
-                        System.out.println(serialPortEvent.getSerialPort().getSystemPortName() + ": " + Arrays.toString(receivedData));
-                    }
-                });
-            }
-        }, 1, TimeUnit.SECONDS);
-
-
+                @Override
+                public void serialEvent(SerialPortEvent serialPortEvent) {
+                    byte[] receivedData = serialPortEvent.getReceivedData();
+                    System.out.println(serialPortEvent.getSerialPort().getSystemPortName() + ": " + new String(receivedData));
+                }
+            });
+        }
+        while (true) {}
     }
 }
